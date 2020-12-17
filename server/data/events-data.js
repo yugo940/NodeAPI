05d06f6a2ecd4ce21fe3f5cf16eed4434c0b0447
yugo940 <kahteran.dino@hotmail.com>
2020-12-17 12:23:26 +0100

    First Commit

---
+++ server/data/events-data.js
@@ -0,0 +1,13 @@
+var events = [{
+    "id": "1",
+    "username": "test",
+    "password": "test"
+},
+{
+    "id": "2",
+    "username": "test2",
+    "password": "test2"
+}
+];
+
+module.exports = events;
